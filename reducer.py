394660e34341843bb70ecfcce0072730412c51a5
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-12-13 02:31:38 -0500

    Corrected and added feature extraction

--- reducer.py
+++ reducer.py
@@ -1,41 +1,59 @@
 # reducer.py
+# Ryan Zembrodt, Thilina Perera
 
-from operator import itemgetter
 import sys
-import nltk
+import zipimport
+importer = zipimport.zipimporter('nltk.mod')
+nltk = importer.load_module('nltk')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer 
 
 FILENAME_IDX = 0
 SENTENCE_INDEX_IDX = 1
 SENTENCE_IDK = 2
 
-#sentences = []
+# Sentiment analyzer
+sid = SentimentIntensityAnalyzer()
+
 # Build a list of tuples of the format: <filename>\t<sentence index>\t<sentence>
 for line in sys.stdin:
     line = line.strip()
     if len(line) > 0:
         try:
+            # Gather filename, chunk index, and senteces from mapper
             filename, chunk_index, sentences = line.strip().split('\t',2)
+            # Split the sentences on backslashes
             for sentence in sentences.split('\\'):
-                print('{}\t{}'.format(filename, sentence))
-            # Convert sentence index to int for sorting
-            #sentences.append((filename, int(sentence_index), sentence))
+                # Extract features from each sentence
+                tokens = nltk.word_tokenize(sentence)
+                tagged = nltk.pos_tag(tokens)
+                ss = sid.polarity_scores(sentence)
+                polarities = []
+                for k in ss:
+                    polarities.append('{},{}'.format(k, ss[k]))
+                #print('{}\t{}\t{}'.format(filename, chunk_index, sentence))
+                print('{}\t{}\t{}\t{}\t{}'.format(
+                    filename, chunk_index, sentence, tagged, polarities))
         except Exception as e:
+            print('FAILURE with line [{}], exception: {}'.format(line, repr(e)))
             continue
 
-# Used to check if we have multiple files on the partition
-"""
-files = set([item[FILENAME_IDX] for item in sentences])
+"""import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer 
+  
+hotel_rev = ["I like going to the cinema.", "I don't like going to the cinema.", "car is green"]
+  
+sid = SentimentIntensityAnalyzer()
+for sentence in hotel_rev:
+     print(sentence)
+     ss = sid.polarity_scores(sentence)
+     for k in ss:
+         print('{0}: {1}, '.format(k, ss[k]), end='')
+     print()
 
-# Sort based on sentence index
-sentences = sorted(sentences, key=itemgetter(SENTENCE_INDEX_IDX))
 
-# Output sentence to partition file, one input file at a time
-for f in files:
-    for sentence in sentences:
-        #tokens = nltk.word_tokenize(str(sentence[SENTENCE_IDK]))
-        #tagged = nltk.pos_tag(tokens)
-        if sentence[FILENAME_IDX] == f:
-            # Output in the format <filename>\t<sentence>
-            #print('{}\t{}\t{}'.format(sentence[FILENAME_IDX], sentence[SENTENCE_IDK],tagged))
-            print('{}\t{}'.format(sentence[FILENAME_IDX], sentence[SENTENCE_IDK]))
+# neg: Negative
+# neu: Neutral
+# pos: Positive
+# compound: Compound (i.e. aggregated score)
+
 """
